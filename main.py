09da21c8a4dcf45df83b5a3ff03229c711c8309f
rachel-labri-tipton <rachemtipton@gmail.com>
2023-04-26 14:55:47 +0200

    basic streamlit functionality working

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ Internal UI for OCF
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 
-import logging
+
 from datetime import datetime, timezone, timedelta 
 from typing import List, Optional
 
@@ -21,177 +21,156 @@ from nowcasting_datamodel.models.gsp import LocationSQL
 from nowcasting_datamodel.models.metric import MetricValueSQL, MetricSQL, MetricValue, DatetimeIntervalSQL
 from get_data import get_metric_value, get_metric, get_datetime_interval
 
-st.sidebar.title("Navigation")
-st.sidebar.subheader("Select Date Range")
-starttime = st.sidebar.date_input("Start Date", datetime(2022, 12, 6))
-endtime = st.sidebar.date_input("End Date", datetime(2023, 1, 2))
 
-#create columns with the actual MAE values for yesterday and today
-
-# select mae for a single day
-
-# ask peter if I'm getting the right number
-
-url = "postgresql://main:lnOgnQV8b9le1liM@localhost:5433/forecastdevelopment"
-connection = DatabaseConnection(url=url, echo=True)
-
+# set up title and subheader
 st.title("OCF Dashboard")
-
-# DATE_TIME = 'created_utc'
-# ID = 'id'
-# DATA_FILE = ('metrics_data.csv')
-
-
-# def load_data():
-#     df = pd.read_csv(DATA_FILE)
-#     return df
+# set up sidebar
+st.sidebar.subheader("Select date range for charts")
+# select start and end date
+starttime = st.sidebar.date_input("Start Date", datetime.today()-timedelta(days=30))
+endtime = st.sidebar.date_input("End Date", datetime.today())
 
 
-
-st.subheader('Daily Latest Mean Absolute Error')
-# st.write('visualizing data from metrics_data.csv')
-
-# def load_data():
-#     df = pd.read_csv(DATA_FILE)
-#     return df
-
-# st.write(load_data())
-
-# def make_line_chart():
-#     df = pd.read_csv(DATA_FILE)
-#     df.columns = df.columns.str.replace("created_utc", "Date")
-#     df.columns = df.columns.str.replace("value", "Value (MW)")
-#     df['Only Date'] = pd.to_datetime(df['Date'])
-    
-#     return df
-
-# st.line_chart(data=make_line_chart(), y="Value (MW)", x="Only Date")
-
-# with st.expander("*Mean Absolute Error"):
-#     st.write("This metric calculates the MAE for the latest OCF forecast and compares with the PVLive values. The data is from one day.")
-
-
-
-
-# # get gsp yields from database
-# with connection.get_session() as session:
-#     # read database
-#     gsp_yields = get_gsp_yield(
-#         session=session,
-#         gsp_ids=range(0, 1),  # could be 0 to 318
-#         start_datetime_utc=(starttime),
-#         end_datetime_utc=(endtime),
-#     )
-
-#     # list of pydantic objects
-#     gsp_yields = [GSPYield.from_orm(gsp_yield) for gsp_yield in gsp_yields]
-
-#     x_pv_live = [gsp_yield.datetime_utc for gsp_yield in gsp_yields]
-#     y_pv_live = [gsp_yield.solar_generation_kw / 1000 for gsp_yield
-#                  in gsp_yields]
-
-# # get latest forecasts from database
-# with connection.get_session() as session:
-#     #read database 
-#     forecasts = get_latest_forecast_for_gsps(
-#         session=session,
-#         gsp_ids=range(0, 1),
-#         historic=True,
-#         start_target_time=datetime(2022, 12, 6),
-#         end_target_time=datetime(2022, 12, 25),
-#     )
-
-#     forecasts = [Forecast.from_orm_latest(forecast) for forecast in forecasts]
-
-
-# x_forecast = [forecast_value.target_time for forecast_value in forecasts[0].forecast_values]
-# y_forecast = [
-#     forecast_value.expected_power_generation_megawatts
-#     for forecast_value in forecasts[0].forecast_values
-# ]
-# df = pd.DataFrame(
-#     {
-#     "x_forecast": x_forecast,
-#     "y_forecast": y_forecast,
-#     }
-# )
-
-# df2 = pd.DataFrame(
-#      {
-#     "x_pv_live": x_pv_live,
-#     "y_pv_live": y_pv_live,
-#     }
-# )
-
-# st.write('Attempts at visualizing data from the database')
-# st.subheader('Dataframe with pv actual numbers and a chart')
-# st.write(df2)
-# st.line_chart(data=df2, x="x_pv_live", y="y_pv_live")
-# st.area_chart(data=df, x="x_forecast", y="y_forecast")
-
-# got an error when I didn't connect to the database so am connecting here again
+# set up database connection
 url = "postgresql://main:lnOgnQV8b9le1liM@localhost:5433/forecastdevelopment"
 connection = DatabaseConnection(url=url, echo=True)
+
+# get metrics for comparing MAE and RMSE without forecast horizon
 
 with connection.get_session() as session:
+    
     # read database metric values 
 
-    metric_values = get_metric_value(
-        session=session,
-        name="Daily Latest MAE",
-        gsp_id=0,
-        start_datetime_utc=starttime,
-        end_datetime_utc=endtime,
-    )
+        metric_values_mae = get_metric_value(
+            session=session,
+            name="Daily Latest MAE",
+            gsp_id=0,
+            start_datetime_utc=starttime,
+            end_datetime_utc=endtime
+            )
 
-
+        metric_values_rmse = get_metric_value(
+            session=session,
+            name="Daily Latest RMSE",
+            gsp_id=0,
+            start_datetime_utc=starttime,
+            end_datetime_utc=endtime
+            )
+    
 
     #transform SQL object into something readable
-    metric_values = [MetricValue.from_orm(value) for value in metric_values]
+        metric_values_mae = [MetricValue.from_orm(value) for value in metric_values_mae]
 
-    #select data to show in the chart mean absolute error and date from the above date range 
-    x_mae = [value.datetime_interval.start_datetime_utc for value in metric_values]
-    y_mae = [round(float(value.value),2) for value in metric_values]
+    #select data to show in the chart MAE and RMSE and date from the above date range 
+        x_mae = [value.datetime_interval.start_datetime_utc for value in metric_values_mae]
+
+        y_mae = [round(float(value.value), 2) for value in metric_values_mae]
+
+#getting recent statistics for the dashboard
+        day_before_yesterday_mae = y_mae[len( y_mae)-3]
+        yesterday_mae = y_mae[len( y_mae)-2] 
+        today_mae =  y_mae[len(y_mae)-1]  
     
-col1, col2 = st.columns([2, 2])
-col1.subheader("Today's MAE")
-col1.metric(label="Today's MAE", value=round(float(y_mae[0]), 2))
+        metric_values_rmse = [MetricValue.from_orm(value) for value in metric_values_rmse]
 
-col2.subheader("Yesterday's MAE")
-col2.metric(label="Yesterday's MAE", value=round(float(y_mae[1]),2))
+        y_rmse = [round(float(value.value), 2) for value in metric_values_rmse]
+    
+        x_rmse = [value.datetime_interval.start_datetime_utc for value in metric_values_rmse]
+        
+# getting recent rmse statistics for the dashboard
+        day_before_yesterday_rmse = y_rmse[len(y_rmse)-3]
+        yesterday_rmse = y_rmse[len(y_rmse)-2] 
+        today_rmse = y_rmse[len(y_rmse)-1] 
 
-st.title("Nowcasting Forecast MAE") 
+# set up title and subheader
+st.title("Nowcasting Forecast Statistics") 
 
-df = pd.DataFrame(
+st.subheader("Recent MAE")
+t = datetime.today()-timedelta(days=1)
+t2 = datetime.today()-timedelta(days=2)
+t3 = datetime.today()-timedelta(days=3)
+col1, col2, col3 = st.columns([1, 1, 1])
+
+col1.metric(label=t3.strftime("%d/%m/%y"), value=day_before_yesterday_mae)
+col2.metric(label=t2.strftime("%d/%m/%y"), value=yesterday_mae)
+col3.metric(label=t.strftime("%d/%m/%y"), value=today_mae)
+
+st.subheader("Recent RMSE")
+col1, col2, col3 = st.columns([1, 1, 1])
+col1.metric(label=t3.strftime("%d/%m/%y"), value=day_before_yesterday_rmse)
+col2.metric(label=t2.strftime("%d/%m/%y"), value=yesterday_rmse)
+col3.metric(label=t.strftime("%d/%m/%y"), value=today_rmse)
+
+df_mae = pd.DataFrame(
     {
     "MAE": y_mae,
-    "date_time_utc": x_mae,
+    "datetime_utc": x_mae,
     }
 )
 
-st.sidebar.subheader("Choose Chart Type")
-chart = st.sidebar.radio("Select", ("Bar Chart", "Line Chart", "Line & Bar Chart"))
-
-if chart == "Bar Chart":
-    fig = px.bar(df, x="date_time_utc", y="MAE", title='MAE Nowcasting Forecast', hover_data=['MAE'], color_discrete_sequence=['#FFD053'],)
-    st.plotly_chart(fig, theme="streamlit")
-elif chart == "Line Chart":
-    fig = px.line(df, x="date_time_utc", y="MAE", title='MAE Nowcasting Forecast', hover_data=['MAE'], color_discrete_sequence=['#FCED4D'],)
-    st.plotly_chart(fig, theme="streamlit")
-else: 
-     fig = px.bar(df, x="date_time_utc", y="MAE", title='MAE Nowcasting Forecast', hover_data=['MAE'], color_discrete_sequence=['#FFD053'],)
-     st.plotly_chart(fig, theme="streamlit")
-
-     fig = px.line(df, x="date_time_utc", y="MAE", title='MAE Nowcasting Forecast', hover_data=['MAE'], color_discrete_sequence=['#FCED4D'],)
-     st.plotly_chart(fig, theme="streamlit")
-   
-
-st.subheader('Raw Data')
-df = pd.DataFrame(
+df_rmse = pd.DataFrame(
     {
-    "MAE": y_mae,
-    "date_time_utc": x_mae,
-     }
+    "RMSE": y_rmse,
+    "datetime_utc": x_rmse,
+    }
 )
 
-st.write(df)
+# st.sidebar.subheader("Select Chart Type")
+# chart = st.sidebar.radio("Select", ("Bar Chart", "Line Chart", "Chart with Forecast Horizon"))
+
+st.sidebar.subheader("Select Forecast Horizon")
+forecast_horizon_selection = st.sidebar.multiselect("Select", [60, 120, 180, 240, 300, 360, 420])
+
+# set up title and subheader
+
+fig = px.bar(df_mae, x="datetime_utc", y="MAE", title='MAE Nowcasting', hover_data=['MAE'], color_discrete_sequence=['#FFD053'],)
+st.plotly_chart(fig, theme="streamlit")
+
+fig2 = px.line(df_mae, x="datetime_utc", y="MAE", title='MAE Nowcasting Forecast', hover_data=['MAE'], color_discrete_sequence=['#FFD053'],)
+
+
+        # fig = px.bar(df_mae, x="datetime_utc", y="MAE", title='MAE Nowcasting Forecast', hover_data=['MAE'], color_discrete_sequence=['#7BCDF3'],)
+        # st.plotly_chart(fig, theme="streamlit")
+    # fig2 = px.line(df_mae, x="datetime_utc", y="MAE", title='Nowcasting MAE with Forecast Horizons', hover_data=['MAE'], color_discrete_sequence=['#FCED4D'],)
+        
+        # st.plotly_chart(fig, theme="streamlit")
+    
+with connection.get_session() as session:
+    # read database metric values
+    for forecast_horizon in forecast_horizon_selection:
+        
+        metric_values = get_metric_value(
+                    session=session,
+                    name="Daily Latest MAE",
+                    gsp_id=0,
+                    forecast_horizon_minutes=forecast_horizon,
+                    start_datetime_utc=starttime,
+                    end_datetime_utc=endtime,
+            )
+        metric_values = [MetricValue.from_orm(value) for value in metric_values]
+        x_mae_horizon = [value.datetime_interval.start_datetime_utc for value in metric_values]
+        y_mae_horizon = [round(float(value.value), 2) for value in metric_values]
+
+        df = pd.DataFrame(
+                {
+                "MAE": y_mae_horizon,
+                "datetime_utc": x_mae_horizon,
+             }
+            )
+        fig2.add_traces([go.Scatter(x=df["datetime_utc"], y=df["MAE"], name=f"{forecast_horizon}-minute horizon",)]) 
+
+    st.plotly_chart(fig2, theme="streamlit")
+
+# comparing MAE and RMSE
+fig3=px.line(df_mae, x="datetime_utc", y="MAE", title='Nowcasting MAE with RMSE for Comparison', hover_data=['MAE'], color_discrete_sequence=['#FFD053', '#FFAC5F', '#ff9736', "#7BCDF3", "#086788", "#63BCAF","#4C9A8E"],)
+
+fig3.add_traces(go.Scatter(x=df_rmse["datetime_utc"], y=df_rmse["RMSE"], name="RMSE",))
+st.plotly_chart(fig3, theme="streamlit")
+# set up title and subheader
+
+st.subheader('Raw Data')
+col1, col2 = st.columns([1, 1])
+col1.write(df_mae)
+col2.write(df_rmse)
+
+# color_discrete_sequence=['#FFD053', '#FFAC5F', '#ff9736', "#7BCDF3", "#086788", "#63BCAF","#4C9A8E"]
