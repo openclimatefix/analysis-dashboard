46de417e67618219a15c145f32220cf0e17d93da
peterdudfield <peter.dudfield@hotmail.com>
2023-07-17 10:30:23 +0100

    add more forecast horizon options

--- src/main.py
+++ src/main.py
@@ -159,7 +159,9 @@ def metric_page():
 
     st.sidebar.subheader("Select Forecast Horizon")
     forecast_horizon_selection = st.sidebar.multiselect(
-        "Select", [0, 60, 120, 180, 240, 300, 360, 420], [60, 120, 240, 420]
+        "Select",
+        [0, 60, 120, 180, 240, 300, 360, 420, 480, 600, 720, 900, 1080, 1440, 1800, 2160],
+        [60, 120, 240, 420]
     )
 
     df_mae = pd.DataFrame(
