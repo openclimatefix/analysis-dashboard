c9fc6b5dfd9e3205c5a3b696a111a2dcf113dda4
rachel-labri-tipton <rachemtipton@gmail.com>
2023-08-24 10:41:15 +0200

    use start_utc instead of created_utc timestamps remove commented out code

--- src/pvsite_forecast.py
+++ src/pvsite_forecast.py
@@ -12,17 +12,6 @@ from pvsite_datamodel.read import (
 import plotly.graph_objects as go
 
 
-colour_per_model = {
-    # "cnn": "#FFD053",
-    # "National_xg": "#7BCDF3",
-    # "pvnet_v2": "#4c9a8e",
-    # "PVLive Initial Estimate": "#e4e4e4",
-    # "PVLive Updated Estimate": "#e4e4e4",
-    # "PVLive GSP Sum Estimate": "#FF9736",
-    # "PVLive GSP Sum Updated": "#FF9736",
-}
-
-
 def pvsite_forecast_page():
     """Main page for pvsite forecast"""
     st.markdown(
@@ -34,8 +23,7 @@ def pvsite_forecast_page():
         unsafe_allow_html=True,
     )
     # get site_uuids from database
-
-    url = os.env("SITES_DB_URL")
+    url = os.environ["SITES_DB_URL"]
     connection = DatabaseConnection(url=url, echo=True)
     with connection.get_session() as session:
         site_uuids = get_all_sites(session=session)
@@ -44,33 +32,37 @@ def pvsite_forecast_page():
         ]
       
     site_selection = st.sidebar.selectbox("Select sites by site_uuid", site_uuids, index=0)
-    starttime = st.sidebar.date_input("Start Date", datetime.today() - timedelta(days=3))
+    starttime = st.sidebar.date_input("Start Date", datetime.today() - timedelta(days=3), max_value=datetime.today())
+    # endtime = st.sidebar.date_input("End Date", datetime.today() + timedelta(days=1))
     st.write("Forecast for", site_selection)
+
     # get forecast values for selected sites and plot
     with connection.get_session() as session:
         forecasts = get_latest_forecast_values_by_site(
             session=session,
             site_uuids=[site_selection],
             start_utc=starttime,
+            # end_utc=endtime,
         )
 
         forecasts = forecasts.values()
-        for forecast in forecasts:
-            for i in forecast: 
-                print(i.forecast_power_kw, i.created_utc)
 
+        for forecast in forecasts:
+            x = [i.start_utc for i in forecast]
+            y = [i.forecast_power_kw for i in forecast]
+
+    # get generation values for selected sites and plot
     with connection.get_session() as session:
         generations = get_pv_generation_by_sites(
             session=session,
             site_uuids=[site_selection],
             start_utc=starttime,
+            # end_utc=endtime
         )
-        # generations = [int(generation.generation_power_kw) for generation in generations if generation is not None]
-        # print("generations", generations)
 
         yy = [generation.generation_power_kw for generation in generations if generation is not None]
-        xx = [generation.created_utc for generation in generations]
-        
+        xx = [generation.start_utc for generation in generations if generation is not None] 
+
     fig = go.Figure(
         layout=go.Layout(
             title=go.layout.Title(text="Latest Forecast for Selected Site"),
@@ -79,10 +71,6 @@ def pvsite_forecast_page():
             legend=go.layout.Legend(title=go.layout.legend.Title(text="Chart Legend")),
         )
     )
-
-    x = [i.created_utc for i in forecast]
-    y = [i.forecast_power_kw for i in forecast]
-    
 
     fig.add_trace(
         go.Scatter(
@@ -97,15 +85,10 @@ def pvsite_forecast_page():
         go.Scatter(
             x=xx,
             y=yy,
-            mode="markers",
+            mode="lines",
             name="selected site generation",
             line=dict(color="#FF9736"),
-    )
+        )
     )
 
     st.plotly_chart(fig, theme="streamlit")
-                
-        
-
-
-    
