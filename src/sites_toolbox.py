dbd8b8a50e504ca40db418ff64bcaa974be8ab17
Rachel Tipton <rachelmtipton@gmail.com>
2023-09-18 12:17:48 +0200

    add more info to section on add site to site group

--- src/sites_toolbox.py
+++ src/sites_toolbox.py
@@ -81,21 +81,27 @@ def get_site_group_details(session, site_group_name):
     )
     site_group_sites = [
         {"site_uuid": str(site.site_uuid), "client_site_id": str(site.client_site_id)}
-        for site in site_group_uuid.sites]
+        for site in site_group_uuid.sites
+    ]
     site_group_users = [user.email for user in site_group_uuid.users]
     return site_group_sites, site_group_users
 
-def update_site_group(session, site_uuid, site_group_name):
-          """Add a site to a site group"""
-          site_group = get_site_group_by_name(session=session, site_group_name=site_group_name)
-          site_group_sites = add_site_to_site_group(session=session, site_uuid=site_uuid, site_group_name=site_group_name)
-          site_group_sites = [
-              {"site_uuid": str(site.site_uuid), "client_site_id": str(site.client_site_id)}
-              for site in site_group.sites]
-          site = get_site_by_uuid(session=session, site_uuid=site_uuid)
-          site_site_groups= [site_group.site_group_name for site_group in site.site_groups]
-          return site_group, site_group_sites, site_site_groups
 
+def update_site_group(session, site_uuid, site_group_name):
+    """Add a site to a site group"""
+    site_group = get_site_group_by_name(
+        session=session, site_group_name=site_group_name
+    )
+    site_group_sites = add_site_to_site_group(
+        session=session, site_uuid=site_uuid, site_group_name=site_group_name
+    )
+    site_group_sites = [
+        {"site_uuid": str(site.site_uuid), "client_site_id": str(site.client_site_id)}
+        for site in site_group.sites
+    ]
+    site = get_site_by_uuid(session=session, site_uuid=site_uuid)
+    site_site_groups = [site_group.site_group_name for site_group in site.site_groups]
+    return site_group, site_group_sites, site_site_groups
 
 
 # sites toolbox page
@@ -166,7 +172,7 @@ def sites_toolbox_page():
         st.write("Here are the site details for site", site_id, ":", site_details)
         if st.button("Close site details"):
             st.empty()
-    
+
     # getting site group details
     st.markdown(
         f'<h1 style="color:#63BCAF;font-size:32px;">{"Get Site Group Details"}</h1>',
@@ -174,23 +180,49 @@ def sites_toolbox_page():
     )
     site_group_name = st.selectbox("Enter the site group name.", site_groups)
     if st.button("Get site group details"):
-        site_group_sites, site_group_users = get_site_group_details(session=session, site_group_name=site_group_name)
-        st.write("Site group", site_group_name, "contains the following", len(site_group_sites), "sites:", site_group_sites)
-        st.write("The following", len(site_group_users), "users are part of this group:", site_group_users)
+        site_group_sites, site_group_users = get_site_group_details(
+            session=session, site_group_name=site_group_name
+        )
+        st.write(
+            "Site group",
+            site_group_name,
+            "contains the following",
+            len(site_group_sites),
+            "sites:",
+            site_group_sites,
+        )
+        st.write(
+            "The following",
+            len(site_group_users),
+            "users are part of this group:",
+            site_group_users,
+        )
         if st.button("Close site group details"):
             st.empty()
-    
+
     # add site to site group
     st.markdown(
         f'<h1 style="color:#63BCAF;font-size:32px;">{"Add Site to Site Group"}</h1>',
         unsafe_allow_html=True,
     )
-    site_uuid = st.selectbox("Select site", site_uuid_list, key="add" )
-    site_group= st.selectbox("Select site group", site_groups)
+    site_uuid = st.selectbox("Select site", site_uuid_list, key="add")
+    site_group = st.selectbox("Select site group", site_groups)
     if st.button("Add site to site group"):
-        site_group, site_group_sites, site_site_groups = update_site_group(session=session, site_uuid=site_uuid, site_group_name=site_group)
-        st.write("Site", site_uuid, "has been added to site group", site_group.site_group_name, "and now contains", len(site_group_sites), "sites.")
-        st.write("This site is part of the following site groups:", site_site_groups )
+        site_group, site_group_sites, site_site_groups = update_site_group(
+            session=session, site_uuid=site_uuid, site_group_name=site_group
+        )
+        st.write(
+            "Site",
+            site_uuid,
+            "has been added to",
+            site_group.site_group_name,
+            ", which has",
+            len(site_group_sites),
+            "sites: ",
+            site_group_sites,
+        )
+        st.write(
+            "The following site groups include site", site_uuid, ":", site_site_groups
+        )
         if st.button("Close details"):
             st.empty()
-  
