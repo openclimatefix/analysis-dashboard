5ed9a2796fb809c317b6051976d012fbbdc9e261
Rachel Tipton <rachelmtipton@gmail.com>
2023-09-18 12:18:50 +0200

    update test add site to site group

--- tests/test_get_data.py
+++ tests/test_get_data.py
@@ -1,12 +1,16 @@
 """tests for get_data.py"""
-from get_data import get_all_users, get_all_site_groups
+from get_data import get_all_users, get_all_site_groups, add_site_to_site_group
+from pvsite_datamodel.sqlmodels import UserSQL, SiteGroupSQL, SiteSQL
 from pvsite_datamodel.read import get_all_sites
+from pvsite_datamodel.write.user_and_site import make_site_group, make_site
 
-#get all users 
+
+# get all users
 def test_get_all_users(db_session):
     users = get_all_users(session=db_session)
     # assert
     assert len(users) == 0
+
 
 # get all sites
 def test_get_all_sites(db_session):
@@ -14,11 +18,13 @@ def test_get_all_sites(db_session):
     # assert
     assert len(sites) == 0
 
+
 # get all site groups
 def test_get_all_site_groups(db_session):
     site_groups = get_all_site_groups(session=db_session)
     # assert
     assert len(site_groups) == 0
+
 
 # add site to site group
 def test_add_site_to_site_group(db_session):
@@ -29,20 +35,10 @@ def test_add_site_to_site_group(db_session):
     site_group.sites.append(site_1)
     site_group.sites.append(site_2)
 
-    site_group = session.query(SiteGroupSQL).filter(SiteGroupSQL.site_group_name == site_group_name).first()
+    add_site_to_site_group(
+        session=db_session,
+        site_uuid=str(site_3.site_uuid),
+        site_group_name=site_group.site_group_name,
+    )
 
-    site = session.query(SiteSQL).filter(SiteSQL.site_uuid == site_uuid).one()
-    print(site_group_name)
-    print(site_group.site_group_name)
-    print(site.site_uuid)
-
-    if site not in site_group.sites:
-
-      site_group.sites.append(site)
-
-    session.commit()
-
-    return site_group.sites
-
-   
-
+    assert len(site_group.sites) == 3
