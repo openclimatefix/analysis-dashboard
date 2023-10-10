f158ed9c03e4e4bf6745c97028060727665bd8f3
peterdudfield <peter.dudfield@hotmail.com>
2023-10-10 09:13:42 +0100

    PR comment

--- tests/plots/test_ramp_rate.py
+++ tests/plots/test_ramp_rate.py
@@ -3,7 +3,7 @@ from plots.ramp_rate import make_ramp_rate_plot
 from datetime import datetime
 
 
-def test_make_pinball_plot(db_session, metrics_ramp_rate):
+def test_make_ramp_rate_plot(db_session, metrics_ramp_rate):
     _ = make_ramp_rate_plot(
         session=db_session,
         starttime=datetime(2023, 1, 1),
