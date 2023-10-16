13e93ef8e82f0c2c691daae5dff8632ac1ddcb97
peterdudfield <peter.dudfield@hotmail.com>
2023-10-16 16:20:11 +0100

    move from side bar

--- src/auth.py
+++ src/auth.py
@@ -13,8 +13,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def check_password():
     """Returns `True` if the user had the correct password."""
 
-    with st.sidebar:
-        user_info = login_button(clientId=clientId, domain=domain,debug_logs=True)
+    user_info = login_button(clientId=clientId, domain=domain,debug_logs=True)
 
     if user_info is None:
         st.text('No user info')
