1fa44446ed4f9c7ea79cb09c9b6064785e90b4aa
peterdudfield <peter.dudfield@hotmail.com>
2024-03-21 20:17:48 +0000

    add plot for frequency of api hits

--- src/plots/users.py
+++ src/plots/users.py
@@ -39,3 +39,39 @@ def make_api_requests_plot(
     )
     fig.update_yaxes(visible=False)
     return fig
+
+
+def make_api_frequency_requests_plot(
+    api_requests: pd.DataFrame, email_selected: str, end_time: datetime, start_time: datetime
+):
+    """
+    Make plot of API frequency requests
+
+    Parameters
+    ----------
+    api_requests : pd.DataFrame, need to have columns "date" and "url"
+    email_selected : str, email of user
+    end_time : datetime, end time of plot
+    start_time : datetime, start time of plot
+
+    """
+
+    fig = go.Figure(
+        layout=go.Layout(
+            title=go.layout.Title(
+                text=f"Number of API requests for {email_selected} between {start_time} and {end_time}"
+            ),
+            xaxis=go.layout.XAxis(title=go.layout.xaxis.Title(text="Date")),
+            yaxis=go.layout.YAxis(title=go.layout.yaxis.Title(text="Numer of API requests")),
+            legend=go.layout.Legend(title=go.layout.legend.Title(text="Chart Legend")),
+        )
+    )
+    fig.add_trace(
+        go.Bar(
+            x=api_requests["date"],
+            y=api_requests["url"],
+            name="API requests",
+        )
+    )
+    fig.update_yaxes(visible=False)
+    return fig
