5135dde56417255b172ef44fe720c7b6e0e62c4f
peterdudfield <peter.dudfield@hotmail.com>
2024-04-24 11:46:06 +0100

    rework sie forecast, to add DA forecast

--- src/pvsite_forecast.py
+++ src/pvsite_forecast.py
@@ -31,19 +31,36 @@ def pvsite_forecast_page():
       
     site_selection = st.sidebar.selectbox("Select sites by site_uuid", site_uuids,)
     starttime = st.sidebar.date_input("Start Date", min_value=datetime.today() - timedelta(days=365), max_value=datetime.today())
-    created = st.sidebar.text_input("Created Before", pd.Timestamp.now().ceil('15min'))
 
-    if created == "":
-        created = datetime.now()
+    forecast_type = st.sidebar.selectbox("Select Forecast Type", ["Latest", "Forecast_horizon", "DA"], 0)
+
+    if forecast_type == "Latest":
+        created = st.sidebar.text_input("Created Before", pd.Timestamp.now().ceil('15min'))
+
+        if created == "":
+            created = datetime.now()
+        else:
+            created = datetime.fromisoformat(created)
+        st.write("Forecast for", site_selection, "starting on", starttime, "created by", created)
     else:
-        created = datetime.fromisoformat(created)
-    st.write("Forecast for", site_selection, "starting on", starttime, "created by", created)
+        created = None
 
-    forecast_horizon = st.sidebar.selectbox("Select Forecast Horizon", range(0,2880,15), None)
+    if forecast_type == "Forecast_horizon":
+        forecast_horizon = st.sidebar.selectbox("Select Forecast Horizon", range(0,2880,15), 5)
+    else:
+        forecast_horizon = None
+
+    if forecast_type == "DA":
+        day_ahead_hours = 9
+        day_ahead_timezone_delta_hours = 5.5
+        st.write(f"Forecast for {day_ahead_hours} oclock the day before "
+                 f"with {day_ahead_timezone_delta_hours} hour timezone delta")
+    else:
+        day_ahead_hours = None
+        day_ahead_timezone_delta_hours = None
 
     # an option to resample to the data
     resample = st.sidebar.selectbox("Resample data", [None, "15T", "30T"], None)
-
 
     # get forecast values for selected sites and plot
     with connection.get_session() as session:
@@ -52,7 +69,9 @@ def pvsite_forecast_page():
             site_uuids=[site_selection],
             start_utc=starttime,
             created_by=created,
-            forecast_horizon_minutes=forecast_horizon
+            forecast_horizon_minutes=forecast_horizon,
+            day_ahead_hours=day_ahead_hours,
+            day_ahead_timezone_delta_hours=day_ahead_timezone_delta_hours
         )
 
         forecasts = forecasts.values()
