aca39bd0b6f87287c26914537e9eaaa43fa562a4
peterdudfield <peter.dudfield@hotmail.com>
2024-11-14 13:59:24 +0000

    update test

--- tests/test_pvsite_location.py
+++ tests/test_pvsite_location.py
@@ -1,5 +1,6 @@
 from pvsite_forecast import calculate_penalty
 import pandas as pd
+import numpy as np
 
 
 def test_calculate_penalty():
@@ -9,13 +10,14 @@ def test_calculate_penalty():
         {
             "datetime": pd.date_range("2021-01-01", periods=5, freq="D"),
             "forecast_power_kw": [0.1, 0.2, 0.3, 0.4, 0.5],
-            "generation_power_kw": [0.2, 0.3, 0.4, 0.5, 0.6],
+            "generation_power_kw": [0.2, 0.3, 0.5, 0.5, 1],
         }
     )
 
     # calculate penalty
-    penalty = calculate_penalty(df)
+    penalty_df, total_penalty = calculate_penalty(df, capacity_kw=2)
+    print(penalty_df)
 
     # check results
     # TODO check result
-    assert penalty == 0.05
+    assert np.round(total_penalty,2) == 0.42
