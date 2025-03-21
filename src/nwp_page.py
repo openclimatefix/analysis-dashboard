57a1db22ca4be1e03268f87741ce05e681bf074b
peterdudfield <peter.dudfield@hotmail.com>
2025-03-21 15:13:53 +0000

    add option for um-ukv

--- src/nwp_page.py
+++ src/nwp_page.py
@@ -153,6 +153,13 @@ def nwp_page():
             xaxis_title = "x_osgb"
             yaxis_title = "y_osgb"
 
+        if "um-ukv" in d_one_channel_one_step.variables:
+            values = d_one_channel_one_step["um-ukv"]
+            x = d_one_channel_one_step.x_laea.values
+            y = d_one_channel_one_step.y_laea.values
+            xaxis_title = "x_laea"
+            yaxis_title = "y_laea"
+
         # reduce dimensions
         if len(values.shape) == 3:
             values = values[0, :, :].values
