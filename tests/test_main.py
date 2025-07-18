e0b8c2697d3e7190f93c1eaba7c1f2cdb35b27b5
peterdudfield <peter.dudfield@hotmail.com>
2025-07-18 11:00:24 +0100

    add to tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,26 @@
 from streamlit.testing.v1 import AppTest
-
+from status import status_page
 
 def test_main_page():
 
     at = AppTest("src/main.py", default_timeout=5)
     at.run()
-    assert at.success
+    assert not at.exception
+
+
+def test_main_india_page():
+
+    at = AppTest("src/main_india.py", default_timeout=5)
+    at.run()
+    assert not at.exception
+
+
+def test_main_status_page():
+    at = AppTest("src/main.py", default_timeout=5)
+    at.run()
+    assert not at.exception
+
+    at.switch_page(page_path="status.py")
+    at.run()
+    assert not at.exception
+
