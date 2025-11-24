c83446eb74b17f6b62e3f22371ef77a15c3e01c9
Peter Dudfield <peter.dudfield@hotmail.com>
2025-11-24 15:44:59 +0000

    forecast vs actual

--- src/dataplatform/forecast/constanst.py
+++ src/dataplatform/forecast/constanst.py
@@ -12,8 +12,8 @@ colours = [
 ]
 
 metrics = {
-    "MAE": "MAE is absolute mean error, average(abs(y-x))",
-    "ME": "ME is mean (bias) error, average((y-x))",
+    "MAE": "MAE is absolute mean error, average(abs(forecast-actual))",
+    "ME": "ME is mean (bias) error, average((forecast-actual))",
     # "TODO NMAE (by capacity)": " NMAE (by capacity), average(abs(y-x)) / mean(capacity)",
     # "TODO NMAE (by mean observed generation)": " NMAE (by mean observed generation), average(abs(y-x)) / mean(y)",
     #    "NMAE (by observed generation)":" NAME (by observed generation)"
